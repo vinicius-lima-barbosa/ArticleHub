9e67b54695562912d3a9ac46f0bb93ebb992b20c
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-06-19 12:08:08 -0300

    feat(models): add user model

---
+++ articlehub-api/internal/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        int       `json:"id" db:"id"`
+	Name      string    `json:"name" db:"name"`
+	Email     string    `json:"email" db:"email"`
+	Password  string    `json:"password" db:"password"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+}
